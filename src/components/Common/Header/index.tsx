6fdd0ff5400cedb8023100ef776cc15ad3b0dbba
juan.garcia <juan.garcia@opinno.com>
2020-04-16 17:38:20 +0200

    [FIX] Header style

--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import {Body, Button, Header, Icon, Left, Title} from "native-base";
+import {Body, Button, Header, Icon, Left} from "native-base";
+import HeaderTitle from "@Custom/HeaderTitle";
 
 export type Props = { title: string, navigation: any }
 
 export const CustomHeader = (props: Props) => {
     const {title, navigation} = props;
     return (
-        <Header>
+        <Header style={{backgroundColor: '#38B1C8'}}>
             <Left style={{flex: 2}}>
                 <Button transparent>
-                    <Icon name="menu" onPress={() => navigation.openDrawer()}/>
+                    <Icon style={{fontSize: 30}} name="menu" onPress={() => navigation.openDrawer()}/>
                 </Button>
             </Left>
             <Body style={{flex: 3}}>
-                <Title>{title}</Title>
+                <HeaderTitle>{title}</HeaderTitle>
             </Body>
         </Header>
     );
---
+++ src/components/Custom/HeaderTitle/index.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+const HeaderTitle = styled.Text`
+	color: white;
+	font-size: 20px;
+`;
+
+export default HeaderTitle;
