a75b8a9c71c8070f95292b22fbdedf4e589600b8
juan.garcia <juan.garcia@opinno.com>
2020-04-20 10:17:24 +0200

    [ADD] Styled components

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,42 +2,30 @@ import React from 'react';
 import {createDrawerNavigator, DrawerItemList} from '@react-navigation/drawer';
 import HomeScreen from "@Screens/HomeScreen";
 import CharactersScreen from "@Screens/CharactersScreen";
-import {Image, View} from "react-native";
+import {View} from "react-native";
 import Logo from "@Assets/logo.jpg";
 import {Icon} from "native-base";
 import LocationsScreen from "@Screens/LocationsScreen";
 import EpisodesScreen from "@Screens/EpisodesScreen";
 import ComparerScreen from "@Screens/ComparerScreen";
+import CustomDivider from "@Custom/Divider";
+import DrawerImage from "@Custom/DrawerImage";
 
 const Drawer = createDrawerNavigator();
 
 const DrawerContent = (children: any) => (
     <>
         <View>
-            <Image
-                style={{
-                    backgroundColor: 'rgb(53, 173, 30)',
-                    height: 120,
-                    width: 280,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-                source={Logo}
-            />
+            <DrawerImage source={Logo}/>
         </View>
-        <View
-            style={{
-                margin: 10,
-                borderBottomColor: '(0, 0, 0, 0.54)',
-                borderBottomWidth: 1,
-            }}
-        />
+        <CustomDivider/>
         <DrawerItemList {...children} />
     </>
 )
 
 export const Routes = () =>
-    <Drawer.Navigator initialRouteName="Home" drawerContent={children => <DrawerContent {...children} />}>
+    <Drawer.Navigator drawerContentOptions={{activeTintColor: '#38B1C8'}} initialRouteName="Home"
+                      drawerContent={children => <DrawerContent {...children} />}>
         <Drawer.Screen name="Home" component={HomeScreen} options={{
             drawerIcon: config => <Icon type={"MaterialCommunityIcons"} name="home"/>
         }}/>
